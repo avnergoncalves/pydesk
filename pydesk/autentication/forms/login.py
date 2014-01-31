15b45c0616864b37e32731e8855a57d3e56692d1
agoncalves <avner.goncalves@s2it.com.br>
2014-01-31 18:33:15 -0200

    Prototipos e Autenticação

---
+++ pydesk/autentication/forms/login.py
@@ -0,0 +1,10 @@
+'''
+Created on Jan 31, 2014
+
+@author: agoncalves
+'''
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField()
--- pydesk/autentication/views.py
+++ pydesk/autentication/views.py
@@ -1,29 +1,35 @@
 from django.shortcuts import render_to_response, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.template.context import RequestContext
+from autentication.forms.login import LoginForm
 
 
 def login(request, user_id=None):
 
+    form = LoginForm()
 
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
         
-        user = authenticate(username=username, password=password)
+        form = LoginForm(request.POST)
         
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                redirect('/dashboard/home')
-            else:
-                # Retorna uma mensagem de erro de 'conta desabilitada' .
-                pass
-        else:
-            # Retorna uma mensagem de erro 'login invalido'.
-            pass
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
 
-    return render_to_response('login/login.html', {}, context_instance=RequestContext(request))
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    redirect('/dashboard/home')
+                else:
+                    # Retorna uma mensagem de erro de 'conta desabilitada' .
+                    pass
+            else:
+                # Retorna uma mensagem de erro 'login invalido'.
+                pass
+
+    return render_to_response('login/login.html', {'form': form}, context_instance=RequestContext(request))
 
 
 def logout(request, user_id=None):
