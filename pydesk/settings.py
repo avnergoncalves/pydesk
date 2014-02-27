4c7a6bf96e327259066ac7d7d4530ab6585e3d2e
agoncalves <avner.goncalves@s2it.com.br>
2014-02-27 16:44:32 -0300

    static

--- pydesk/settings.py
+++ pydesk/settings.py
@@ -94,8 +94,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
 
 
 #**************************************************************************************************
